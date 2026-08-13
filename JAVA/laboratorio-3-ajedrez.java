9216227ce04d3b5f3cff12aa06710a2f47b48bde
Jeremy Quesada <quesadajeremy7@gmail.com>
2026-08-12 21:41:50 -0600

    Refactor chessboard logic and method names
    Refactor methods for clarity and functionality, including renaming and restructuring. Improved logic for generating and displaying the chessboard.

--- JAVA/laboratorio-3-ajedrez.java
+++ JAVA/laboratorio-3-ajedrez.java
@@ -5,13 +5,12 @@
 package laboratorio3andrésaguilera;
 
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  *
  * @author aagui
  */
-public class Laboratorio3AndrésAguilera {
+public class Laboratorio3AndrésAguilera  {
 
     static final int FILAS = 8;
     static final int COLUMNAS = 8;
@@ -19,37 +18,139 @@ public class Laboratorio3AndrésAguilera {
     static int filaCaballo = 0;
     static int columnaCaballo = 0;
 
-    //Matriz Tablero de Ajedrez
+    //Matriz
     static int[][] tablero = new int[FILAS][COLUMNAS];
 
     static Random random = new Random();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        IniciarTablero();
-        MostrarTablero();
-        System.out.println("Caballo esta en la fila " + filaCaballo + " Caballo esta en columna " + columnaCaballo);
+    // Mejor resultado encontrado
+    static int[][] mejorTablero = new int[FILAS][COLUMNAS];
 
+    static int mejorCantidadMensajes = 0;
+    static int mejorCantidadMovimientos = 0;
+    static int mejorIntento = 0;
+
+    static int mejorFilaInicial = 0;
+    static int mejorColumnaInicial = 0;
+
+    static int mejorFilaFinal = 0;
+    static int mejorColumnaFinal = 0;
+
+    public static void main(String[] args) {
+
+        int intentos = 0;
+        boolean recorridoCompleto = false;
+
+        // Realizamos hasta 200 intentos
+        while (intentos < 200 && recorridoCompleto == false) {
+
+            intentos++;
+
+            // Generamos un tablero nuevo
+            generarTablero();
+
+            // Guardamos la posición inicial
+            int filaInicial = filaCaballo;
+            int columnaInicial = columnaCaballo;
+
+            // Ejecutamos el recorrido
+            int[] resultado = ejecutarRecorrido();
+
+            int mensajesEntregados = resultado[0];
+            int movimientosRealizados = resultado[1];
+
+            // Revisamos si se entregaron los 8 mensajes
+            if (mensajesEntregados == 8) {
+
+                recorridoCompleto = true;
+
+                mejorTablero = copiarMatriz(tablero);
+
+                mejorCantidadMensajes = mensajesEntregados;
+                mejorCantidadMovimientos = movimientosRealizados;
+
+                mejorIntento = intentos;
+
+                mejorFilaInicial = filaInicial;
+                mejorColumnaInicial = columnaInicial;
+
+                mejorFilaFinal = filaCaballo;
+                mejorColumnaFinal = columnaCaballo;
+
+            } else if (mensajesEntregados > mejorCantidadMensajes) {
+
+                // Guardamos el intento que entregó más mensajes
+                mejorTablero = copiarMatriz(tablero);
+
+                mejorCantidadMensajes = mensajesEntregados;
+                mejorCantidadMovimientos = movimientosRealizados;
+
+                mejorIntento = intentos;
+
+                mejorFilaInicial = filaInicial;
+                mejorColumnaInicial = columnaInicial;
+
+                mejorFilaFinal = filaCaballo;
+                mejorColumnaFinal = columnaCaballo;
+            }
+        }
+
+        System.out.println();
+        System.out.println("Final:");
+
+        if (recorridoCompleto) {
+            System.out.println("Se completo el recorrido.");
+        } else {
+            System.out.println("No se completo el recorrido.");
+        }
+
+        System.out.println("Intentos realizados: " + intentos);
+        System.out.println("Mensajes entregados: " + mejorCantidadMensajes);
+        System.out.println("Movimientos realizados: " + mejorCantidadMovimientos);
+
+        System.out.println("Posicion inicial: fila "
+                + (mejorFilaInicial + 1)
+                + ", columna "
+                + (mejorColumnaInicial + 1));
+
+        System.out.println("Posicion final: fila "
+                + (mejorFilaFinal + 1)
+                + ", columna "
+                + (mejorColumnaFinal + 1));
+
+        System.out.println("Mejor intento: " + mejorIntento);
+
+        System.out.println();
+        System.out.println("TABLERO FINAL");
+
+        mostrarTablero(mejorTablero);
     }
 
-    //Iniciar metodo tablero
-    public static void IniciarTablero() {
+    // Metodo generarTablero
+    public static void generarTablero() {
+
+        // Limpiamos el tablero
         for (int i = 0; i < FILAS; i++) {
+
             for (int j = 0; j < COLUMNAS; j++) {
+
                 tablero[i][j] = 0;
             }
         }
-        filaCaballo = random.nextInt(FILAS);
-
-        columnaCaballo = random.nextInt(COLUMNAS);
 
         //Casilla bloqueada
         for (int i = 0; i < 6; i++) {
-            tablero[random.nextInt(FILAS)][random.nextInt(COLUMNAS)] = -1;
+            int fila = random.nextInt(FILAS);
+            int columna = random.nextInt(COLUMNAS);
+            // Mientras la casilla no este libre
+            while (tablero[fila][columna] != 0) {
+                fila = random.nextInt(FILAS);
+                columna = random.nextInt(COLUMNAS);
+            }
+
+            tablero[fila][columna] = -1;
         }
+
         //Casilla con mensaje
         for (int i = 0; i < 8; i++) {
             int filaValida = random.nextInt(FILAS);
@@ -62,105 +163,186 @@ public class Laboratorio3AndrésAguilera {
             }
             tablero[filaValida][columnaValida] = -2;
         }
+
+        // Colocar el caballo
+        filaCaballo = random.nextInt(FILAS);
+        columnaCaballo = random.nextInt(COLUMNAS);
+        while (tablero[filaCaballo][columnaCaballo] != 0) {
+            filaCaballo = random.nextInt(FILAS);
+            columnaCaballo = random.nextInt(COLUMNAS);
+        }
+        tablero[filaCaballo][columnaCaballo] = 1;
     }
 
-    //Metodo mostrar tablero 
-    public static void MostrarTablero() {
+    //Metodo mostrarTablero
+    public static void mostrarTablero(int[][] matriz) {
+
         System.out.println();
+
         for (int i = 0; i < FILAS; i++) {
             for (int j = 0; j < COLUMNAS; j++) {
-                System.out.print(tablero[i][j] + " ");
+                System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
         }
-
     }
 
-    //Metodo es Movimiento Valido
+    // Metodo esMovimientoValido
     public static boolean esMovimientoValido(int fila, int columna) {
 
-        if (fila >= FILAS || fila <= -1) {
+        //fila este dentro del tablero
+        if (fila < 0 || fila >= FILAS) {
             return false;
         }
 
-        if (columna >= COLUMNAS || columna <= -1) {
+        //columna este dentro del tablero
+        if (columna < 0 || columna >= COLUMNAS) {
             return false;
         }
 
-        if (tablero[fila][columna] > 0 || tablero[fila][columna] == -1) {
+        // -1 significa casilla bloqueada
+        if (tablero[fila][columna] == -1) {
+            return false;
+        }
+
+        //casilla visitada
+        if (tablero[fila][columna] > 0) {
             return false;
         }
 
         return true;
     }
 
-    //Metodo ejecutar recorrido 
-    public static int contarMovimientosDisponible(int fila, int columna) {
+    //Metodo contarMovimientosDisponibles
+    public static int contarMovimientosDisponibles(int fila, int columna) {
+        int movimientosValidos = 0;
 
-        int movimientoValidos = 0;
+        // Los 8 movimientos posibles del caballo
         int[] movimientoFila = {-2, -1, 1, 2, 2, 1, -1, -2};
         int[] movimientoColumna = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        // Revisamos los 8 movimientos
         for (int i = 0; i < 8; i++) {
             int nuevaFila = fila + movimientoFila[i];
             int nuevaColumna = columna + movimientoColumna[i];
 
-            if (esMovimientoValido(nuevaFila, nuevaColumna) == true) {
-                movimientoValidos = i++;
-
+            if (esMovimientoValido(nuevaFila, nuevaColumna)) {
+                movimientosValidos++;
             }
-
         }
-        return movimientoValidos;
 
+        return movimientosValidos;
     }
 
-    //Ejecutar recorrido
-    public static void ejecutarRecorrido() {
+    //Metodo seleccionarMovimiento
+    public static int seleccionarMovimiento() {
 
-        int[] filasCandidatas = new int[8];
-        int[] columnasCandidatas = new int[8];
-        int menoresMovimientos = 9;
-        int posiblesCandidatos = 0;
-
-        int mensajesEntregados = 0;
-        int movimientosRealizados = 0;
-        boolean hayMovimientosValidos = true;
-
+        // Los 8 movimientos posibles del caballo
         int[] movimientoFila = {-2, -1, 1, 2, 2, 1, -1, -2};
         int[] movimientoColumna = {1, 2, 2, 1, -1, -2, -2, -1};
 
-        while (mensajesEntregados < 8 && hayMovimientosValidos) {
-            
-            menoresMovimientos = 9;
-            posiblesCandidatos = 0;
-            
-            for (int i = 0; i < 8; i++) {
-                int nuevaFila = filaCaballo + movimientoFila[i];
-                int nuevaColumna = columnaCaballo + movimientoColumna[i];
+        // Vectores para guardar los candidatos
+        int[] filasCandidatas = new int[8];
+        int[] columnasCandidatas = new int[8];
 
-                if (esMovimientoValido(nuevaFila, nuevaColumna)) {
-                    int disponibles = contarMovimientosDisponible(nuevaFila, nuevaColumna); // 2
+        int menorCantidad = 9;
+        int cantidadCandidatos = 0;
 
-                    if (disponibles < menoresMovimientos) {
-                        menoresMovimientos = disponibles;
-                        posiblesCandidatos = 0;
-                        filasCandidatas[posiblesCandidatos] = nuevaFila;
-                        columnasCandidatas[posiblesCandidatos] = nuevaColumna;
-                        posiblesCandidatos++;
-                    }else if(disponibles == menoresMovimientos){
-                        filasCandidatas[posiblesCandidatos] = nuevaFila;
-                        columnasCandidatas[posiblesCandidatos] = nuevaColumna;
-                        posiblesCandidatos++;  
-                    }
+        // Revisamos los 8 movimientos
+        for (int i = 0; i < 8; i++) {
+
+            int nuevaFila = filaCaballo + movimientoFila[i];
+            int nuevaColumna = columnaCaballo + movimientoColumna[i];
+
+            //movimiento es posible
+            if (esMovimientoValido(nuevaFila, nuevaColumna)) {
+                int disponibles = contarMovimientosDisponibles(nuevaFila, nuevaColumna);
+
+                if (disponibles < menorCantidad) {
+                    menorCantidad = disponibles;
+                    cantidadCandidatos = 0;
+                    filasCandidatas[cantidadCandidatos] = nuevaFila;
+                    columnasCandidatas[cantidadCandidatos] = nuevaColumna;
+                    cantidadCandidatos++;
+
+                } else if (disponibles == menorCantidad) {
+                    filasCandidatas[cantidadCandidatos] = nuevaFila;
+                    columnasCandidatas[cantidadCandidatos] = nuevaColumna;
+                    cantidadCandidatos++;
                 }
-
             }
-            if (posiblesCandidatos == 0){
-                hayMovimientosValidos = false;
-            }
-
         }
 
+        // Si no hay movimientos validos
+        if (cantidadCandidatos == 0) {
+            return 0;
+        }
+
+        //Seleccionar aleatoriamente uno de los candidatos
+        int posicion = random.nextInt(cantidadCandidatos);
+
+        filaCaballo = filasCandidatas[posicion];
+        columnaCaballo = columnasCandidatas[posicion];
+
+        return 1;
     }
 
+    //Metodo copiarMatriz
+    public static int[][] copiarMatriz(int[][] matriz) {
+
+        int[][] copia = new int[FILAS][COLUMNAS];
+
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+                copia[i][j] = matriz[i][j];
+            }
+        }
+
+        return copia;
+    }
+
+    //Metodo ejecutarRecorrido
+    public static int[] ejecutarRecorrido() {
+
+        int mensajesEntregados = 0;
+        int movimientosRealizados = 0;
+
+        int numeroVisita = 1;
+
+        boolean hayMovimientos = true;
+
+        //Bucle para repetir busqueda de movimientos
+        while (mensajesEntregados < 8 && hayMovimientos) {
+
+            // Seleccionamos el siguiente movimiento
+            int movimiento = seleccionarMovimiento();
+
+            // Si no existe movimiento valido
+            if (movimiento == 0) {
+                hayMovimientos = false;
+
+            } else {
+
+                movimientosRealizados++;
+
+                numeroVisita++;
+
+                //contar mensajes
+                if (tablero[filaCaballo][columnaCaballo] == -2) {
+                    mensajesEntregados++;
+                }
+
+                //Orden de la visita
+                tablero[filaCaballo][columnaCaballo] = numeroVisita;
+            }
+        }
+
+        // Retornamos los resultados
+        int[] resultado = new int[2];
+
+        resultado[0] = mensajesEntregados;
+        resultado[1] = movimientosRealizados;
+
+        return resultado;
+    }
 }
